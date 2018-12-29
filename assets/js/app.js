fcd92567546768c38d0401189382faaa09985e5c
kw7oe <choongkwern@hotmail.com>
2018-12-29 17:40:26 +0800

    Add js to auto resize textarea

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,14 @@ import socket from "./socket"
 import Post from "./post"
 
 Post.init(socket, document.getElementById("preview"))
+
+var tx = document.getElementsByTagName('textarea');
+for (var i = 0; i < tx.length; i++) {
+  tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+}
