823fc944700fd91d2f0fbc97e4a06e49fa383347
kw7oe <choongkwern@hotmail.com>
2018-12-29 18:14:11 +0800

    Hide edit delete links if not owner

--- lib/til_web/views/post_view.ex
+++ lib/til_web/views/post_view.ex
@@ -1,3 +1,8 @@
 defmodule TilWeb.PostView do
   use TilWeb, :view
+
+  def is_owner(nil, post), do: false
+  def is_owner(current_user, post) do
+    current_user.id == post.credential_id
+  end
 end
