cf9a9a3e2474fee6ecaa41df683c41b80dcaa9a8
kw7oe <choongkwern@hotmail.com>
2018-12-31 08:44:54 +0800

    Update production host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :til, TilWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "electric-filthy-jerboa.gigalixirapp.com", port: 80],
+  url: [host: "til.kaiwern.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
