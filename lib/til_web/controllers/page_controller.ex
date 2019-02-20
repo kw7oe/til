ac147d38abbfa7712f9724e9ffcda31609893f11
kw7oe <choongkwern@hotmail.com>
2019-02-20 10:37:53 +0800

    Add filter form in index page

--- lib/til_web/controllers/page_controller.ex
+++ lib/til_web/controllers/page_controller.ex
@@ -6,8 +6,9 @@ defmodule TilWeb.PageController do
   def index(conn, params) do
     tags = Map.get(params, "tags", "")
            |> String.split(",", trim: true)
+    all_tags = Posts.list_tags() |> Enum.map(&(&1.name))
 
     posts = Posts.list_posts(tags)
-    render(conn, "index.html", posts: posts)
+    render(conn, "index.html", posts: posts, tags: all_tags)
   end
 end
