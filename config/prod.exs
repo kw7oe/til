99961e880e106790f4fdb190df130af8f2f1f5dd
kw7oe <choongkwern@hotmail.com>
2019-05-07 16:21:12 +0800

    Configure prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :til, TilWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [:inet6, port: System.get_env("PORT")],
   url: [scheme: "https", host: "til.kaiwern.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
