bd8ffeb607dd180dbc9e3f354dd9ed7463fdb421
kw7oe <choongkwern@hotmail.com>
2019-05-07 15:46:27 +0800

    Config prod

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,10 @@ use Mix.Config
 config :til, TilWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "til.kaiwern.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:togethere, :vsn)
 
 # Do not print debug messages in production
 config :logger, level: :info
